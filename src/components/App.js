aed67b4c6a800881ba2965b33dedea3d3e21e710
chaitanyapvk1993 <1993chaitanyapvk@gmail.com>
2021-05-27 16:24:12 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,20 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-
 class App extends Component {
     constructor(props) {
 		super(props);
+	   this.state={
+	     isClicked:false
+	    }
 	};
-	onDishSelect() {
-		var but=document.getElementById("click");
-		var para = document.createElement("P");   
-		para.setAttribute("id","para");              
-var t = document.createTextNode("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");     
-para.appendChild(t);           
-but.appendChild(para)   ;                            
-
-
-        
-    }
-
     render() {
     	return(
     		<div id="main">
 				{ /* Do not remove this main div!! */ }
-				<button  id="click" onClick={() => this.onDishSelect()}></button>
+		{this.state.isClicked?<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>:null}
+		<button id="click" onClick={()=>{this.setState({isClicked:true})}}>click</button>
     		</div>
     	);
     }
 }
-
-
 export default App;
-
